46e15de8eb909c1a0b2a0040040b1168e7ceb054
Konstantin Voykov <koykov0@gmail.com>
2025-02-03 01:27:17 +0300

    bitarray: cover with test/bench

--- bitarray.go
+++ bitarray.go
@@ -6,22 +6,26 @@ type bitarray struct {
 	buf []byte
 }
 
-func (b *bitarray) prealloc(size uint) {
+func (b *bitarray) prealloc(size uint) *bitarray {
 	b.buf = make([]byte, size/8+1)
+	return b
 }
 
-func (b *bitarray) set(i int) {
+func (b *bitarray) set(i int) *bitarray {
 	b.buf[i/8] |= 1 << (i % 8)
+	return b
 }
 
-func (b *bitarray) clear(i int) {
+func (b *bitarray) clear(i int) *bitarray {
 	b.buf[i/8] &^= 1 << (i % 8)
+	return b
 }
 
-func (b *bitarray) get(i int) bool {
-	return b.buf[i/8]&(1<<(i%8)) != 0
+func (b *bitarray) get(i int) uint8 {
+	return (b.buf[i/8] & (1 << (i % 8))) >> (i % 8)
 }
 
-func (b *bitarray) reset() {
+func (b *bitarray) reset() *bitarray {
 	openrt.Memclr(b.buf)
+	return b
 }
