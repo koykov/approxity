bf58a70c7aa342417a5d909361e5b66b44cdd7d6
Konstantin Voykov <koykov0@gmail.com>
2025-02-06 23:48:50 +0300

    implement testing hashers

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/koykov/bloom
 
 go 1.18
 
-require github.com/koykov/bitvector v0.0.0-20250204230211-0b2acbb44595
+require (
+	github.com/koykov/bitvector v0.0.0-20250204230211-0b2acbb44595
+	github.com/koykov/byteconv v1.0.1
+)
 
 require github.com/koykov/openrt v0.0.0-20240728195600-ccc3242946f9 // indirect
---
+++ hasher_test.go
@@ -0,0 +1,49 @@
+package bloom
+
+import (
+	"hash/crc32"
+	"hash/crc64"
+
+	"github.com/koykov/byteconv"
+)
+
+type hasherStringCRC32 struct{}
+
+func (hasherStringCRC32) Hash(data any) uint64 {
+	switch x := data.(type) {
+	case string:
+		return uint64(crc32.ChecksumIEEE(byteconv.S2B(x)))
+	case *string:
+		return uint64(crc32.ChecksumIEEE(byteconv.S2B(*x)))
+	case []byte:
+		return uint64(crc32.ChecksumIEEE(x))
+	case *[]byte:
+		return uint64(crc32.ChecksumIEEE(*x))
+	}
+	return 0
+}
+
+type hasherStringCRC64 struct {
+	poly  uint64
+	table *crc64.Table
+}
+
+func (h *hasherStringCRC64) Hash(data any) uint64 {
+	if h.poly == 0 {
+		h.poly = crc64.ISO
+	}
+	if h.table == nil {
+		h.table = crc64.MakeTable(h.poly)
+	}
+	switch x := data.(type) {
+	case string:
+		return crc64.Checksum(byteconv.S2B(x), h.table)
+	case *string:
+		return crc64.Checksum(byteconv.S2B(*x), h.table)
+	case []byte:
+		return crc64.Checksum(x, h.table)
+	case *[]byte:
+		return crc64.Checksum(*x, h.table)
+	}
+	return 0
+}
