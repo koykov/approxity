2807f7fc09e547f6b44dd269c216867aa297077b
Konstantin Voykov <koykov0@gmail.com>
2025-03-05 00:14:32 +0300

    hyperloglog: alloc-free bias calculation

--- hyperloglog/bias.go
+++ hyperloglog/bias.go
@@ -4,58 +4,12 @@ import (
 	"encoding/binary"
 	"math"
 	"os"
-	"sort"
 	"unsafe"
 )
 
 // empirical bias correction pairs
 // loads from local binary due to huge size
 var bias [15][][2]float64
-
-func biasfn(p uint64, e float64) float64 {
-	_ = bias[14]
-	const k = 6 // K-nn
-	var a [96]byte
-	keys := *(*[k][2]float64)(unsafe.Pointer(&a))
-	// var keys [k][2]float64
-	ssize := len(bias[p])
-	var eidx int
-	{
-		// lower_bound
-		for i := 0; i < ssize; i++ {
-			if bias[p][i][0] < e {
-				eidx = i
-				continue
-			}
-			break
-		}
-	}
-	{
-		// partial_sort_copy
-		lo, hi := 0, ssize
-		if eidx > k {
-			lo = eidx - k
-		}
-		if eidx+k < ssize {
-			hi = eidx + k
-		}
-		for i := lo; i < hi; i++ {
-			keys[i-lo][0], keys[i-lo][1] = bias[p][i][0], bias[p][i][1]
-		}
-		sort.Slice(keys[:], func(i, j int) bool {
-			return math.Abs(keys[i][0]-e) < math.Abs(keys[j][0]-e)
-		})
-	}
-	var s, ws float64
-	{
-		// accumulate
-		for i := 0; i < k; i++ {
-			s += keys[i][1] * 1 / (math.Abs(keys[i][0]-e) + 1e-5)
-			ws += 1 / (math.Abs(keys[i][0]-e) + 1e-5)
-		}
-	}
-	return s / ws
-}
 
 func init() {
 	fh, err := os.OpenFile("bias.bin", os.O_RDONLY, 0644)
@@ -80,5 +34,76 @@ func init() {
 			bias_ := binary.LittleEndian.Uint64(buf[:])
 			bias[i] = append(bias[i], [2]float64{math.Float64frombits(dist), math.Float64frombits(bias_)})
 		}
+	}
+}
+
+type biasp struct{ d, e float64 }
+
+func biasfn(p uint64, e float64) float64 {
+	_ = bias[14]
+	const k = 6 // K-nn
+	var a [96]byte
+	keys := *(*[6]biasp)(unsafe.Pointer(&a))
+	ssize := len(bias[p])
+	var eidx int
+	{
+		// std::lower_bound
+		for i := 0; i < ssize; i++ {
+			if bias[p][i][0] < e {
+				eidx = i
+				continue
+			}
+			break
+		}
+	}
+	{
+		// std::partial_sort_copy
+		lo, hi := 0, ssize
+		if eidx > k {
+			lo = eidx - k
+		}
+		if eidx+k < ssize {
+			hi = eidx + k
+		}
+		for i := lo; i < hi; i++ {
+			keys[i-lo].d, keys[i-lo].e = bias[p][i][0], bias[p][i][1]
+		}
+		biasQsort(keys[:], 0, k-1, e)
+	}
+	var s, ws float64
+	{
+		// std::accumulate
+		for i := 0; i < k; i++ {
+			s += keys[i].e * 1 / (math.Abs(keys[i].d-e) + 1e-5)
+			ws += 1 / (math.Abs(keys[i].d-e) + 1e-5)
+		}
+	}
+	return s / ws
+}
+
+func biasPivot(p []biasp, lo, hi int, e float64) int {
+	if len(p) == 0 {
+		return 0
+	}
+	pi := &p[hi]
+	i := lo - 1
+	_ = p[len(p)-1]
+	for j := lo; j <= hi-1; j++ {
+		if p[j].d-e < pi.d-e {
+			i++
+			p[i], p[j] = p[j], p[i]
+		}
+	}
+	if i < hi {
+		p[i+1], p[hi] = p[hi], p[i+1]
+	}
+	return i + 1
+}
+
+func biasQsort(p []biasp, lo, hi int, e float64) {
+	if lo < hi {
+		pi := biasPivot(p, lo, hi, e)
+		biasQsort(p, lo, pi-1, e)
+		biasQsort(p, pi+1, hi, e)
 	}
 }
