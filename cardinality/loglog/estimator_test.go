91c01302707fdeaa339de409b286a28c874bb3f5
Konstantin Voykov <koykov0@gmail.com>
2025-03-15 21:11:14 +0300

    cardinality: loglog: cover reader/writer with tests

--- cardinality/loglog/estimator_test.go
+++ cardinality/loglog/estimator_test.go
@@ -1,6 +1,7 @@
 package loglog
 
 import (
+	"os"
 	"testing"
 
 	"github.com/koykov/approxity/cardinality"
@@ -29,6 +30,61 @@ func TestEstimator(t *testing.T) {
 			t.Fatal(err)
 		}
 		cardinality.TestMeConcurrently(t, est, testD)
+	})
+	t.Run("writer", func(t *testing.T) {
+		testWrite := func(t *testing.T, est cardinality.Estimator[string], path string, expect int64) {
+			_ = est.Add("foobar")
+			for i := 0; i < 100; i++ {
+				_ = est.Add("qwerty")
+			}
+			fh, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer func() { _ = fh.Close() }()
+			n, err := est.WriteTo(fh)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if n != expect {
+				t.Fatalf("expected %d bytes, got %d", expect, n)
+			}
+		}
+		t.Run("sync", func(t *testing.T) {
+			f, _ := NewEstimator[string](NewConfig(testP, testh))
+			testWrite(t, f, "testdata/estimator.bin", 262176)
+		})
+		t.Run("concurrent", func(t *testing.T) {
+			f, _ := NewEstimator[string](NewConfig(testP, testh).WithConcurrency())
+			testWrite(t, f, "testdata/concurrent_estimator.bin", 262176)
+		})
+	})
+	t.Run("reader", func(t *testing.T) {
+		testRead := func(t *testing.T, est cardinality.Estimator[string], path string, expectBytes int64, expectEst uint64) {
+			fh, err := os.OpenFile(path, os.O_RDONLY, 0644)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer func() { _ = fh.Close() }()
+			n, err := est.ReadFrom(fh)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if n != expectBytes {
+				t.Fatalf("expected %d bytes, got %d", expectBytes, n)
+			}
+			if e := est.Estimate(); e != expectEst {
+				t.Errorf("expected %d estimate, got %d", expectEst, e)
+			}
+		}
+		t.Run("sync", func(t *testing.T) {
+			f, _ := NewEstimator[string](NewConfig(testP, testh))
+			testRead(t, f, "testdata/estimator.bin", 262176, 2)
+		})
+		t.Run("concurrent", func(t *testing.T) {
+			f, _ := NewEstimator[string](NewConfig(testP, testh).WithConcurrency())
+			testRead(t, f, "testdata/concurrent_estimator.bin", 262176, 2)
+		})
 	})
 }
 
