d61b5cf52e57a5b06b047690291de39ab8a10abd
Konstantin Voykov <koykov0@gmail.com>
2025-03-17 23:44:15 +0300

    cardinality: implement dummy metrics writer

--- cardinality/metrics.go
+++ cardinality/metrics.go
@@ -4,3 +4,8 @@ type MetricsWriter interface {
 	Add(error) error
 	Estimate(uint64) uint64
 }
+
+type DummyMetricsWriter struct{}
+
+func (w DummyMetricsWriter) Add(err error) error      { return err }
+func (w DummyMetricsWriter) Estimate(n uint64) uint64 { return n }
