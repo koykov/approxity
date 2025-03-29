795a349bd05a2f6ff12548a7eafd66e101d8c829
Konstantin Voykov <koykov0@gmail.com>
2025-03-30 00:34:29 +0300

    frequency: countsketch: config comment issues; add concurrent IO test/bench

--- frequency/countsketch/config.go
+++ frequency/countsketch/config.go
@@ -6,15 +6,31 @@ import (
 )
 
 type Config struct {
-	Confidence    float64
-	Epsilon       float64
-	Compact       bool
-	Hasher        approxity.Hasher
-	Concurrent    *ConcurrentConfig
+	// Confidence represent a possibility that potential error will be in range of acceptable error rate (see Epsilon).
+	// E.g.: Confidence 0.99 guarantees 99% of estimations will be in range represent by Epsilon.
+	// Must be in range (0..1).
+	// Mandatory param.
+	Confidence float64
+	// Epsilon represent precision of estimation: less epsilon value makes estimation more accurate, but grows the table.
+	// E.g.: Epsilon 0.01 guarantees that estimation error is less or equal 1% of all elements.
+	// Must be in range (0..1).
+	// Mandatory param.
+	Epsilon float64
+	// Hasher to calculate hash sum of the items.
+	// Mandatory param.
+	Hasher approxity.Hasher
+	// Enable compact mode.
+	// By default, uses 64 bit per counter. This param allows to use 32 bit per counter.
+	Compact bool
+	// Setting up this section enables concurrent read/write operations.
+	Concurrent *ConcurrentConfig
+	// Metrics writer handler.
 	MetricsWriter frequency.MetricsWriter
 }
 
+// ConcurrentConfig configures concurrent section of config.
 type ConcurrentConfig struct {
+	// How many write attempts may perform.
 	WriteAttemptsLimit uint64
 }
 
@@ -24,6 +40,29 @@ func NewConfig(confidence, epsilon float64, hasher approxity.Hasher) *Config {
 		Epsilon:    epsilon,
 		Hasher:     hasher,
 	}
+}
+
+func (c *Config) WithConcurrency() *Config {
+	c.Concurrent = &ConcurrentConfig{}
+	return c
+}
+
+func (c *Config) WithWriteAttemptsLimit(limit uint64) *Config {
+	if c.Concurrent == nil {
+		c.Concurrent = &ConcurrentConfig{}
+	}
+	c.Concurrent.WriteAttemptsLimit = limit
+	return c
+}
+
+func (c *Config) WithCompact() *Config {
+	c.Compact = true
+	return c
+}
+
+func WithMetricsWriter(conf *Config, mw frequency.MetricsWriter) *Config {
+	conf.MetricsWriter = mw
+	return conf
 }
 
 func (c *Config) copy() *Config {
--- frequency/countsketch/estimator_test.go
+++ frequency/countsketch/estimator_test.go
@@ -22,4 +22,30 @@ func TestEstimator(t *testing.T) {
 		}
 		frequency.TestMe(t, frequency.NewTestSignedAdapter(est))
 	})
+	t.Run("concurrent", func(t *testing.T) {
+		est, err := NewEstimator[[]byte](NewConfig(testConfidence, testEpsilon, testh).
+			WithConcurrency())
+		if err != nil {
+			t.Fatal(err)
+		}
+		frequency.TestMeConcurrently(t, frequency.NewTestSignedAdapter(est))
+	})
+}
+
+func BenchmarkEstimator(b *testing.B) {
+	b.Run("sync", func(b *testing.B) {
+		est, err := NewEstimator[[]byte](NewConfig(testConfidence, testEpsilon, testh))
+		if err != nil {
+			b.Fatal(err)
+		}
+		frequency.BenchMe(b, frequency.NewTestSignedAdapter(est))
+	})
+	b.Run("concurrent", func(b *testing.B) {
+		est, err := NewEstimator[[]byte](NewConfig(testConfidence, testEpsilon, testh).
+			WithConcurrency().WithWriteAttemptsLimit(5))
+		if err != nil {
+			b.Fatal(err)
+		}
+		frequency.BenchMeConcurrently(b, frequency.NewTestSignedAdapter(est))
+	})
 }
