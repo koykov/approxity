31305c055588f5e2980ecb74658f22cb7aa67339
Konstantin Voykov <koykov0@gmail.com>
2025-04-25 00:00:48 +0300

    shingle: word: implement method Each

--- shingle/word.go
+++ shingle/word.go
@@ -43,7 +43,19 @@ func (sh *word[T]) AppendShingle(dst []T, s T) []T {
 }
 
 func (sh *word[T]) Each(s T, fn func(T)) {
-	// todo implement me
+	b := sh.clean(s, true)
+	sc := byteseq.B2Q[T](b)
+	if len(b) < 2 {
+		fn(sc)
+		return
+	}
+	lo, hi := 0, sh.k
+	_, _ = sh.spc[len(sh.spc)-1], sc[len(sc)-1]
+	for i := uint64(0); i < uint64(len(sh.spc))-uint64(sh.k); i++ {
+		fn(sc[sh.spc[lo]:sh.spc[hi]])
+		lo++
+		hi++
+	}
 }
 
 func (sh *word[T]) Reset() {
