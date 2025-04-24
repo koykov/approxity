b3a8e1f09ed5307f6b768e82af30ea3d22047799
Konstantin Voykov <koykov0@gmail.com>
2025-04-24 23:54:18 +0300

    shingle: improve hbtrie.contains

--- shingle/hbtrie.go
+++ shingle/hbtrie.go
@@ -13,19 +13,19 @@ func (t *hbtrie) set(r rune) {
 
 func (t *hbtrie) contains(r rune) bool {
 	b0, b1, b2, b3 := uint8(r>>24), uint8(r>>16), uint8(r>>8), uint8(r)
-	if t[b0/64]&(1<<(b0%64)) == 0 {
-		return false
-	}
-	if t[4+b1/64]&(1<<(b1%64)) == 0 {
-		return false
-	}
-	if t[8+b2/64]&(1<<(b2%64)) == 0 {
-		return false
-	}
-	if t[12+b3/64]&(1<<(b3%64)) == 0 {
-		return false
-	}
-	return true
+
+	m0 := uint64(1 << (b0 % 64))
+	m1 := uint64(1 << (b1 % 64))
+	m2 := uint64(1 << (b2 % 64))
+	m3 := uint64(1 << (b3 % 64))
+
+	_ = t[15]
+	l0 := t[b0/64]
+	l1 := t[4+b1/64]
+	l2 := t[8+b2/64]
+	l3 := t[12+b3/64]
+
+	return (l0&m0 != 0) && (l1&m1 != 0) && (l2&m2 != 0) && (l3&m3 != 0)
 }
 
 func (t *hbtrie) reset() {
