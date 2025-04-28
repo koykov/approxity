7a415f338ce1e7cfe7064c6793a0207d74009437
Konstantin Voykov <koykov0@gmail.com>
2025-04-28 21:53:39 +0300

    lsh: bbitminhash: follow minhash.Vector interface

--- lsh/bbitminhash/bbitvec.go
+++ lsh/bbitminhash/bbitvec.go
@@ -12,7 +12,19 @@ func newBbitvec(b uint64) *bbitvec {
 	return &bbitvec{b: b}
 }
 
-func (v *bbitvec) add(val uint64) {
+func (v *bbitvec) Grow(cap_ uint64) {
+	total := cap_ * v.b
+	sz := (total + 63) / 64
+
+	if uint64(cap(v.buf)) < sz {
+		nbuf := make([]uint64, sz)
+		copy(nbuf, v.buf)
+		v.buf = nbuf
+	}
+	v.c = cap_
+}
+
+func (v *bbitvec) Add(val uint64) {
 	lo := val & ((1 << v.b) - 1)
 
 	// position in buf
@@ -32,7 +44,6 @@ func (v *bbitvec) add(val uint64) {
 		remainings := 64 - boff
 		v.buf[idx] |= lo << boff
 
-		// 2. Оставшиеся биты записываем в следующий элемент
 		// - rest of bits moves to the next element
 		next := lo >> remainings
 		v.buf = append(v.buf, next)
@@ -42,9 +53,70 @@ func (v *bbitvec) add(val uint64) {
 	v.c++
 }
 
-func (v *bbitvec) each(fn func(uint64)) {
+func (v *bbitvec) SetMin(pos, val uint64) {
+	if curr := v.Get(pos); val < curr {
+		v.set(pos, val)
+	}
+}
+
+func (v *bbitvec) Get(pos uint64) uint64 {
+	if pos >= v.c {
+		return 0
+	}
+
+	bpos := pos * v.b
+	idx := bpos / 64
+	boff := bpos % 64
+
+	if boff+v.b <= 64 {
+		return (v.buf[idx] >> boff) & ((1 << v.b) - 1)
+	}
+
+	rem := 64 - boff
+	lo := v.buf[idx] >> boff
+	hi := v.buf[idx+1] & ((1 << (v.b - rem)) - 1)
+	return lo | (hi << rem)
+}
+
+func (v *bbitvec) Memset(val uint64) {
 	if v.c == 0 {
 		return
+	}
+
+	pattern := val & ((1 << v.b) - 1)
+	pattern = pattern | (pattern << v.b)
+	pattern = pattern | (pattern << (2 * v.b))
+	for i := uint64(0); i < v.c; i++ {
+		v.set(i, pattern)
+	}
+}
+
+func (v *bbitvec) set(pos uint64, val uint64) {
+	bpos := pos * v.b
+	idx := bpos / 64
+	boff := bpos % 64
+	mask := uint64(((1 << v.b) - 1) << boff)
+
+	// clear current bits
+	v.buf[idx] &^= mask
+	// write new bits
+	v.buf[idx] |= (val & ((1 << v.b) - 1)) << boff
+
+	if boff+v.b > 64 {
+		rem := v.b - (64 - boff)
+		next := idx + 1
+		if next >= uint64(len(v.buf)) {
+			v.buf = append(v.buf, 0)
+		}
+
+		v.buf[next] &^= (1 << rem) - 1
+		v.buf[next] |= val >> (64 - boff)
+	}
+}
+
+func (v *bbitvec) AppendAll(dst []uint64) []uint64 {
+	if v.c == 0 {
+		return dst
 	}
 
 	var (
@@ -71,7 +143,6 @@ func (v *bbitvec) each(fn func(uint64)) {
 			value := (v.buf[idx] >> shift) & mask
 			curr |= value << (v.b - bleft)
 
-			// Обновляем позиции
 			bpos += readBits
 			bleft -= readBits
 
@@ -80,11 +151,16 @@ func (v *bbitvec) each(fn func(uint64)) {
 			}
 		}
 
-		fn(curr)
+		dst = append(dst, curr)
 	}
+	return dst
 }
 
-func (v *bbitvec) reset() {
+func (v *bbitvec) Len() uint64 {
+	return v.c
+}
+
+func (v *bbitvec) Reset() {
 	v.buf = v.buf[:0]
 	v.off = 0
 	v.c = 0
--- lsh/bbitminhash/bbitvec_test.go
+++ lsh/bbitminhash/bbitvec_test.go
@@ -1,21 +1,43 @@
 package bbitminhash
 
-import "testing"
+import (
+	"math"
+	"reflect"
+	"testing"
+)
 
 var bbvTestVals = []uint64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
 
 func TestBbitvec(t *testing.T) {
 	vec := newBbitvec(5)
-	for _, v := range bbvTestVals {
-		vec.add(v)
+
+	vec.Grow(21)
+	if vec.Len() != 21 {
+		t.Errorf("expected 20, got %d", vec.Len())
 	}
-	var c uint64
-	vec.each(func(v uint64) {
-		if v != c {
-			t.Errorf("expected %d, got %d", c, v)
+
+	vec.Memset(12)
+	for i := 0; i < len(bbvTestVals); i++ {
+		if vec.Get(uint64(i)) != 12 {
+			t.Errorf("pos %d expected 12, got %d", i, vec.Get(uint64(i)))
 		}
-		c++
-	})
+	}
+
+	vec.Reset()
+	for i := 0; i < len(bbvTestVals); i++ {
+		if vec.Get(uint64(i)) != 0 {
+			t.Errorf("expected 0, got %d", vec.Get(uint64(i)))
+		}
+	}
+
+	for _, v := range bbvTestVals {
+		vec.Add(v)
+	}
+	var x []uint64
+	x = vec.AppendAll(x)
+	if !reflect.DeepEqual(x, bbvTestVals) {
+		t.Errorf("expected %v, got %v", bbvTestVals, x)
+	}
 }
 
 func BenchmarkBbitvec(b *testing.B) {
@@ -23,20 +45,46 @@ func BenchmarkBbitvec(b *testing.B) {
 		vec := newBbitvec(5)
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
-			vec.reset()
+			vec.Reset()
 			for _, v := range bbvTestVals {
-				vec.add(v)
+				vec.Add(v)
 			}
 		}
 	})
-	b.Run("each", func(b *testing.B) {
+	b.Run("set", func(b *testing.B) {
 		vec := newBbitvec(5)
-		for _, v := range bbvTestVals {
-			vec.add(v)
-		}
+		vec.Grow(10)
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
-			vec.each(func(v uint64) {})
+			vec.SetMin(3, math.MaxUint32)
+		}
+	})
+	b.Run("get", func(b *testing.B) {
+		vec := newBbitvec(5)
+		vec.Memset(100)
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			vec.Get(3)
+		}
+	})
+	b.Run("append all", func(b *testing.B) {
+		vec := newBbitvec(5)
+		for _, v := range bbvTestVals {
+			vec.Add(v)
+		}
+		var x []uint64
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			x = vec.AppendAll(x[:0])
+			_ = x
+		}
+	})
+	b.Run("memset", func(b *testing.B) {
+		vec := newBbitvec(5)
+		vec.Grow(10)
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			vec.Memset(math.MaxUint64)
 		}
 	})
 }
