0c79148fac3923c497c537cb980ad7e32029b3a5
Konstantin Voykov <koykov0@gmail.com>
2025-04-30 23:06:42 +0300

    similarity: cosine: implement full algorithm

--- similarity/cosine/estimator.go
+++ similarity/cosine/estimator.go
@@ -1,6 +1,7 @@
 package cosine
 
 import (
+	"math"
 	"sync"
 
 	"github.com/koykov/byteseq"
@@ -23,28 +24,49 @@ func NewEstimator[T byteseq.Q](conf *Config[T]) (similarity.Estimator[T], error)
 	return e, nil
 }
 
-func (e *estimator[T]) Estimate(a, b T) (_ float64, err error) {
+func (e *estimator[T]) Estimate(a, b T) (r float64, err error) {
 	if e.once.Do(e.init); e.err != nil {
-		return 0, e.err
+		err = e.err
+		return
 	}
 	var mid int
 	if err = e.conf.LSH.Add(a); err != nil {
-		return 0, err
+		return
 	}
 	e.buf = e.conf.LSH.AppendHash(e.buf[:0])
 	mid = len(e.buf)
 
 	if err = e.conf.LSH.Add(b); err != nil {
-		return 0, err
+		return
 	}
 	e.buf = e.conf.LSH.AppendHash(e.buf)
 
 	abuf, bbuf := e.buf[:mid], e.buf[mid:]
-	n := min(len(abuf), len(bbuf))
-	for i := 0; i < n; i++ {
-		// todo calculate...
+	if len(abuf) == 0 || len(bbuf) == 0 {
+		return
 	}
-	return 0.0, nil
+	var amag, bmag float64
+	n := max(len(abuf), len(bbuf))
+	_, _ = abuf[len(abuf)-1], bbuf[len(bbuf)-1]
+	for i := 0; i < n; i++ {
+		var av, ab float64
+		if i < len(abuf) {
+			av = float64(abuf[i])
+		}
+		if i < len(bbuf) {
+			ab = float64(bbuf[i])
+		}
+		r += av * ab
+		amag += av * av
+		bmag += ab * ab
+	}
+	amag = math.Sqrt(amag)
+	bmag = math.Sqrt(bmag)
+	if amag == 0 || bmag == 0 {
+		return
+	}
+	r /= amag * bmag
+	return
 }
 
 func (e *estimator[T]) Reset() {
