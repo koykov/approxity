bc99fff9dc9ad2deeca65be6e189534adef60f16
Konstantin Voykov <koykov0@gmail.com>
2025-05-14 23:52:36 +0300

    heavy: common testing approach; apply for spacesaving

--- heavy/spacesaving/hitter_test.go
+++ heavy/spacesaving/hitter_test.go
@@ -1,11 +1,9 @@
 package spacesaving
 
 import (
-	"math/rand"
 	"testing"
 
 	"github.com/koykov/hash/xxhash"
-	"github.com/koykov/pbtk"
 	"github.com/koykov/pbtk/heavy"
 )
 
@@ -15,68 +13,19 @@ var (
 )
 
 func TestHitter(t *testing.T) {
-	pbtk.EachTestingDataset(func(_ int, ds *pbtk.TestingDataset[[]byte]) {
-		t.Run(ds.Name, func(t *testing.T) {
-			if len(ds.All) <= 20 {
-				return
-			}
-			h, err := NewHitter[[]byte](NewConfig(5, testh).
-				WithEWMA(testAlpha))
-			if err != nil {
-				t.Fatal(err)
-			}
-			repeat := len(ds.All) / 20
-			for i := 0; i < len(ds.All); i++ {
-				if err = h.Add(ds.All[i]); err != nil {
-					t.Fatal(err)
-				}
-				if i%repeat == 0 {
-					j := rand.Intn(6)
-					_ = h.Add(ds.All[j])
-				}
-			}
-			hits := h.Hits()
-			r := make(map[string]float64, len(hits))
-			for i := 0; i < len(hits); i++ {
-				r[string(hits[i].Key)] = hits[i].Rate
-			}
-			t.Logf("%#v", r)
-		})
-	})
+	h, err := NewHitter[[]byte](NewConfig(5, testh).
+		WithEWMA(testAlpha))
+	if err != nil {
+		t.Fatal(err)
+	}
+	heavy.TestMe(t, h)
 }
 
 func BenchmarkHitter(b *testing.B) {
-	pbtk.EachTestingDataset(func(_ int, ds *pbtk.TestingDataset[[]byte]) {
-		b.Run(ds.Name, func(b *testing.B) {
-			h, err := NewHitter[[]byte](NewConfig(5, testh).
-				WithEWMA(testAlpha))
-			if err != nil {
-				b.Fatal(err)
-			}
-			b.Run("add", func(b *testing.B) {
-				b.ReportAllocs()
-				b.ResetTimer()
-				for i := 0; i < b.N; i++ {
-					if err = h.Add(ds.All[i%len(ds.All)]); err != nil {
-						b.Fatal(err)
-					}
-				}
-			})
-			b.Run("hits", func(b *testing.B) {
-				h.Reset()
-				for i := 0; i < len(ds.All); i++ {
-					if err = h.Add(ds.All[i]); err != nil {
-						b.Fatal(err)
-					}
-				}
-				b.ReportAllocs()
-				b.ResetTimer()
-				var buf []heavy.Hit[[]byte]
-				for i := 0; i < b.N; i++ {
-					buf = h.AppendHits(buf[:0])
-				}
-				_ = buf
-			})
-		})
-	})
+	h, err := NewHitter[[]byte](NewConfig(5, testh).
+		WithEWMA(testAlpha))
+	if err != nil {
+		b.Fatal(err)
+	}
+	heavy.BenchMe(b, h)
 }
---
+++ heavy/testing.go
@@ -0,0 +1,67 @@
+package heavy
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/koykov/pbtk"
+)
+
+func TestMe[T []byte](t *testing.T, h Hitter[T]) {
+	pbtk.EachTestingDataset(func(_ int, ds *pbtk.TestingDataset[[]byte]) {
+		t.Run(ds.Name, func(t *testing.T) {
+			h.Reset()
+			if len(ds.All) <= 20 {
+				return
+			}
+			repeat := len(ds.All) / 20
+			for i := 0; i < len(ds.All); i++ {
+				if err := h.Add(ds.All[i]); err != nil {
+					t.Fatal(err)
+				}
+				if i%repeat == 0 {
+					j := rand.Intn(6)
+					_ = h.Add(ds.All[j])
+				}
+			}
+			hits := h.Hits()
+			r := make(map[string]float64, len(hits))
+			for i := 0; i < len(hits); i++ {
+				r[string(hits[i].Key)] = hits[i].Rate
+			}
+			t.Logf("%v", r)
+		})
+	})
+}
+
+func BenchMe(b *testing.B, h Hitter[[]byte]) {
+	pbtk.EachTestingDataset(func(_ int, ds *pbtk.TestingDataset[[]byte]) {
+		b.Run(ds.Name, func(b *testing.B) {
+			b.Run("add", func(b *testing.B) {
+				h.Reset()
+				b.ReportAllocs()
+				b.ResetTimer()
+				for i := 0; i < b.N; i++ {
+					if err := h.Add(ds.All[i%len(ds.All)]); err != nil {
+						b.Fatal(err)
+					}
+				}
+			})
+			b.Run("hits", func(b *testing.B) {
+				h.Reset()
+				for i := 0; i < len(ds.All); i++ {
+					if err := h.Add(ds.All[i]); err != nil {
+						b.Fatal(err)
+					}
+				}
+				b.ReportAllocs()
+				b.ResetTimer()
+				var buf []Hit[[]byte]
+				for i := 0; i < b.N; i++ {
+					buf = h.AppendHits(buf[:0])
+				}
+				_ = buf
+			})
+		})
+	})
+}
